a75e533869e889d0dbb2f3f181ebddddbe1040b8
chaitan54 <megavathchaitan@gmail.com>
2021-03-24 04:39:05 +0530

    done

--- app.js
+++ app.js
@@ -1,51 +1,51 @@
-const express= require('express');
-const app=express();
+const express= require('express')
 const request=require('request')
-const dotenv = require('dotenv');
-dotenv.config();
-app.set("view engine", "ejs");
-app.use('/public', express.static('public'));
-app.get("/", (req, res)=>{
-    res.render("Home");
-});
-app.get("/result", (req, res)=>{
-    const query = req.query.search;
-    const url = "http://www.omdbapi.com/?apikey=cfd672ef&s=" + query;
-    request(url, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            const data = JSON.parse(body)
-          
-            if(data.Response==='False'){
-                res.send("Movie Not Found");
-            }else{
-                res.render("Result", {data:data});    
-            }
-        }else{
-            res.send('Error');
-        }
-    });
-});
-app.get("/result/:id", (req, res)=>{
-    const url = "http://www.omdbapi.com/?i=tt3896198&apikey=a3da473" + req.params.id;
-    request(url, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            const data = JSON.parse(body)
-           
-            if(data.Response==='False'){
-                res.send("Movie Not Found");
-            }else{
-               
-                res.render("Info", {movie: data});    
-            }
-        }else{
-            res.send('Error');
-        }
-    });
-});
-app.get("*", (req, res)=>{
-    res.send("Some Error");
-})
+//Syntax of express
+const app= express()
+const dotenv=require('dotenv')
+dotenv.config()
+app.set("view engine", "ejs")
+app.use('/public', express.static('public'))
 
-app.listen('80',()=>{
-    console.log("server started")
+app.get("/", (req, res)=>{
+    res.render("homepage")
+})
+app.get("/aboutme", (req, res)=>{
+    res.render("Aboutme")
+})
+app.get("/result", (req, res)=>{
+    console.log(req.query.movieName)
+    const url= `http://www.omdbapi.com/?apikey=cfd672ef&s=${req.query.movieName}`
+    request(url, function (error, response, body){
+        if(!error && response.statusCode==200){
+            const data= JSON.parse(body)
+            //console.log(data)
+            //res.send(data)
+            res.render("result", {movieData: data})
+        }else{
+            res.send("Uh oh error")
+        }
+    })
+})
+app.get("/result/:id", (req, res)=>{
+    //console.log(req.query.movieName)
+    console.log(process.env.API_KEY)
+    const url= `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&i=${req.params.id}`
+    request(url, function (error, response, body){
+        if(!error && response.statusCode==200){
+            const data= JSON.parse(body)
+            //console.log(data)
+            //res.send(data)
+            res.render("AboutMovie", {movieData: data})
+        }else{
+            res.send("Uh oh error")
+        }
+    })
+})
+app.get("*", (req, res)=>{
+    res.send("Go back! Illegal response")
+})
+//We are creating
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server has started at port ${process.env.PORT}`)
 })
